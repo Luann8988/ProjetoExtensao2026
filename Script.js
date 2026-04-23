2d92c42b7f9b820930ab1b36d387c37fc57412e7
Luann <luann.85937100@gmail.com>
2026-04-23 10:38:12 -0300

    Fix merge conflict in Script.js
    Removed conflict markers and cleaned up the showProfile function.

--- Script.js
+++ Script.js
@@ -706,9 +706,6 @@ function submitFeedback() {
 function showProfile() {
     alert("Funcionalidade de edição de perfil em desenvolvimento!");
 }
-<<<<<<< HEAD
-=======
 
->>>>>>> afdc6210b376cff5c8a7bfbd125d18c347533fa5
 
 
